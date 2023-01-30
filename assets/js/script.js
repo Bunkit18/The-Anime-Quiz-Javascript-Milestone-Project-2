77e4c0837aef47125f061ae7409fdb88d0414b6e
Bunkit18 <jessswork18@gmail.com>
2023-01-30 20:37:44 +0000

    Added css styling to instructs and added js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 // Home Div assigned to home variable for easy access and usage
 let home = document.getElementById("home");
 
-
-let instructs = document.getElementById("instructs");
+//add event listener to how to play button
 let instructions = document.getElementById("instructions");
-console.log(instructions);
-
 instructions.addEventListener("click", howToPlay);
 
+let instructHome = document.getElementById("instruct-home");
+instructHome.addEventListener("click", returnHome);
+
 function howToPlay() {
+    let instructs = document.getElementById("instructs");
+    
     home.hidden = true;
     instructs.hidden = false;
+}
+
+function returnHome() {
+    home.hidden = false;
+    instructs.hidden = true;
 }
 
 
