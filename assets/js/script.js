1b78d3a105dd53fcb67d140f76de4001766567f7
Bunkit18 <jessswork18@gmail.com>
2023-02-03 11:17:39 +0000

    Undid test changes to JS shuffle and removed functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,6 +137,14 @@ function checkAnswer() {
     }
 }
 
+function correct(corAns) {
+    corAns.style.backgroundColor = "Green";
+}
+
+function incorrect(inAns) {
+    inAns.style.backgroundColor = "Red";
+}
+
 function incrementScore() {
     let score = 0;
     score += 10;
