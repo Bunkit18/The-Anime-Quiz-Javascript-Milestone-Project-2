b9842df6d6cb68f56d1ce297f25a178d592be4ad
Bunkit18 <jessswork18@gmail.com>
2023-02-06 21:34:52 +0000

    Added html and css for end of quiz modal box, js functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,11 @@ startQuiz.addEventListener("click", showQuizSection);
 let quizHome = document.getElementById("logo");
 quizHome.addEventListener("click", returnHome);
 
+//Variable to store the index number for iteration
 let currentQuestionIndex = 0;
+
+//Keeps track of the score
+let score = 0;
 
 // Question and answer variable, nested array/object within array
 const quizQA = [
@@ -31,63 +35,63 @@ const quizQA = [
     },
     
     {
-        question: "Who is Rin Okumura's father in Blue Exocist?",
+        question: "Who is Rin Okumura's father?",
         answers: ["Shiro Fujimoto", "Satan", "Yukio Okumura", "Igor Neuhaus"],
         correctAnswer: "Satan",
         img: "assets/images/rin.jpg"
     },
     
     {
-        question: "What is the name of the guild Kirito joins in Sword Art Online?",
+        question: "What is the name of the guild Kirito joins?",
         answers: ["Moonlit Black Cats", "Knights of the Blood", "Aincrad Liberation Force", "Titan's Hand"],
         correctAnswer: "Moonlit Black Cats",
         img: "assets/images/kirito.png"
     },
 
     {
-        question: "Who is NOT a member of the Future Gadget Lab in Steins;Gate?",
+        question: "Who is NOT a member of Future Gadget Lab?",
         answers: ["Itaru Hashida", "Kiryuu Moeka", "Kurisu Makise", "Yugo Tennouji"],
         correctAnswer: "Yugo Tennouji",
         img: "assets/images/steins.png"
     }, 
 
     {
-        question: "What is the name of Midoriya Izuku's quirk in My Hero Academia?",
+        question: "What is the name of Midoriya Izuku's quirk?",
         answers: ["All for One", "Bruiser", "Charging Muscles", "One for All"],
         correctAnswer: "One for All",
         img: "assets/images/izuku.jpg"
     },
 
     {
-        question: "How many Sukuna's fingers did Itadori Yuji eat in Jujutsu Kaisen?",
+        question: "How many Sukuna's fingers did Itadori Yuji eat?",
         answers: ["3", "4", "6", "2"],
         correctAnswer: "4",
         img: "assets/images/yuji.jpg"
     },
 
     {
-        question: "What colour is Violet's brooch in Violet Evergarden?",
+        question: "What colour is Violet's brooch?",
         answers: ["Red", "Blue", "Green", "Purple"],
         correctAnswer: "Green",
         img: "assets/images/violet.jpg"
     },
 
     {
-        question: "What is the name of the ophanage 'Mama' in the Promised Neverland?",
+        question: "What is the name of the ophanage 'Mama'?",
         answers: ["Gilda", "Isabella", "Emma", "Viviana"],
         correctAnswer: "Isabella",
         img: "assets/images/isabella.jpg"
     },
 
     {
-        question: "What is the name of Kaneki Ken's kagune in Tokyo Ghoul?",
+        question: "What is the name of Kaneki Ken's kagune?",
         answers: ["Koukaku", "Bikaku", "Chimera", "Rinkaku"],
         correctAnswer: "Rinkaku",
         img: "assets/images/kaneki.jpg"
     },
 
     {
-        question: "What is the name of Eren Yaeger's first Titan in Attack on Titan?",
+        question: "What is the name of Eren Yaeger's first Titan?",
         answers: ["Attack Titan", "Colossal Titan", "Founding Day Titan", "Armored Titan"],
         correctAnswer: "Attack Titan",
         img: "assets/images/AoT.jpg"
@@ -109,6 +113,8 @@ function returnHome() {
     home.hidden = false;
     instructs.hidden = true;
     quiz.hidden = true;
+    currentQuestionIndex = 0;
+    document.getElementById("score").textContent = 0;
 }
 
 /**
@@ -116,6 +122,7 @@ function returnHome() {
  */
 function showQuizSection() {
     // toggle visibility
+    score = 0;
     home.hidden = true;
     instructs.hidden = true;
     quiz.hidden = false;
@@ -123,7 +130,15 @@ function showQuizSection() {
     addQuestionAnswers();
 }
 
+/**
+ * Iterates the next index questions, image and answers into DOM elements
+ * Adds event listener to all buttons and calls the checkAnswer function
+ */
 function addQuestionAnswers() {
+    if (currentQuestionIndex === quizQA.length) {
+        displayModal();
+    }
+
     // accesses DOM elements and assigns to variables
     let questions = document.getElementById("questions");
     let answers = document.querySelectorAll("#answers > button");
@@ -135,10 +150,17 @@ function addQuestionAnswers() {
     for (let j = 0; j < 4; j++) {
         //iterates answers into button text content
         answers[j].textContent = quizQA[currentQuestionIndex].answers[j];
+        answers[j].style.backgroundColor = "#464089";
         answers[j].addEventListener("click", checkAnswer);
     }
 } 
 
+/**
+ * Checks the textContent of the button clicked on and acts accordingly:
+ * Changes buttons backgroundColor depending on correctAnswer
+ * Adds 1 to checkQuestionIndex
+ * Adds an event listener to next-question button
+ */
 function checkAnswer() {
     
     if (this.textContent === quizQA[currentQuestionIndex].correctAnswer) {
@@ -154,67 +176,24 @@ function checkAnswer() {
         }
     }
     currentQuestionIndex++;
-    document.getElementById("next-question").hidden = false;
+
+    let nextQuestion = document.getElementById("next-question");
+    nextQuestion.addEventListener("click", addQuestionAnswers);
 }
 
-// if (currentQuestionIndex === quizQA.length) {
-//     return;
-// } else {
-//     btn.style.backgroundColor = "#464089";
-// }
-
 /**
- * Toggles the quiz screen section to visible and makes the all other sections hidden
- * Iterates and fills html sections with contents of const array
+ * Adds 10 to the score and puts the content into score-box
  */
-// function playQuiz() {
-    
-//     showQuiz();
-
-//     // accesses DOM elements and assigns to variables
-//     let questions = document.getElementById("questions");
-//     let answers = document.querySelectorAll("#answers > button");
-//     let imageSpace = document.getElementById("image");
-
-//     // iterates through quizQA object and fills the question h1 element and answers elements using quizQA array
-//     for (let i = 0; i < quizQA.length; i++) {
-//         questions.textContent = quizQA[i].question;
-//         imageSpace.src = quizQA[i].img;
-//         for (let j = 0; j < 4; j++) {
-//             //iterates answers into button text content
-//             answers[j].textContent = quizQA[i].answers[j];
-
-//             //adds event listener to each button and performs check on the button thats clicked against correctAnswer.
-//             //changes backgroundColor accordingly
-//             answers[j].addEventListener("click", function() {
-//                 if (this.innerText === quizQA[i].correctAnswer) {
-//                     this.style.backgroundColor = "green";
-//                     incrementScore();
-//                     // wait for 1 second and then move to the next question
-//                     setTimeout(function() {
-//                         if (i === quizQA.length) {
-//                             return;
-//                         } else {
-//                             {
-//                                 btn.style.backgroundColor = "#464089";
-//                             }
-//                         }
-//                     }, 1000);
-//                 } else {
-//                     this.style.backgroundColor = "red";
-//                 }
-//             });
-
-//         }
-//         break;
-//     }
-// }
-
 function incrementScore() {
-    let score = 0;
-    score += 10;
-    let scoreBox = document.getElementById("score");
-    scoreBox.textContent = `${score}`;
+    if (score < 100){
+        score += 10;
+        let scoreBox = document.getElementById("score");
+        scoreBox.textContent = `${score}`;
+    }
+}
+
+function displayModal() {
+    
 }
 
 
